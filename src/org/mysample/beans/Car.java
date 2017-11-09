383aba694dee77e775b48b676a84886bc41bc9c8
Ashwani Kuntal <akuntal@csc.com>
2017-11-09 19:18:02 +0530

    Added Singleton Prop File

--- src/org/mysample/beans/Car.java
+++ src/org/mysample/beans/Car.java
@@ -1,15 +1,12 @@
 package org.mysample.beans;
 
-import org.mysample.utilities.MyappApplicationContext;
-import org.springframework.context.ApplicationContext;
-
 public class Car implements Vehicle {	
 	
 	@Override
 	public void drive() {
 		System.out.println("Driving Car");
-		ApplicationContext appcontext = MyappApplicationContext.getApplicationContext();
+		/*ApplicationContext appcontext = MyappApplicationContext.getApplicationContext();
 		Vehicle veh = (Vehicle) appcontext.getBean("motorbike");
-		veh.drive();
+		veh.drive();*/
 	}
 }
--- src/org/mysample/main/RunApp.java
+++ src/org/mysample/main/RunApp.java
@@ -1,14 +1,20 @@
 package org.mysample.main;
 
 import org.mysample.beans.Vehicle;
+import org.mysample.utilities.SpringSingelton;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class RunApp {
 	
+	static ApplicationContext appContext;
+	static {
+		appContext = SpringSingelton.getInstance().getAppContext();
+	}
+	
 	public static void main(String[] args) {
 		
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("SpringContext.xml");
+		SpringSingelton.getInstance();
 		Vehicle veh = (Vehicle) appContext.getBean("car");
 		veh.drive();
 		
